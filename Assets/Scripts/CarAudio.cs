2aec623f26012f45abb498c8b75543ed31fe2e58
Timothy Nicholson <91631726+timmy-2003@users.noreply.github.com>
2023-01-21 18:38:45 +0100

    audio source for music that can be mutable with key: M music still missing

--- Assets/Scripts/CarAudio.cs
+++ Assets/Scripts/CarAudio.cs
@@ -6,6 +6,7 @@ public class CarAudio : MonoBehaviour
 {
     Rigidbody rb;
     public AudioSource engineSource;
+    public AudioSource musicSource;
 
     public float minEnginePitch = 0.2f;
     public float maxEnginePitch = 3.0f;
@@ -18,6 +19,11 @@ public class CarAudio : MonoBehaviour
 
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            musicSource.mute = !musicSource.mute;
+        }
         float speed = rb.velocity.magnitude;
         engineSource.pitch = minEnginePitch + (speed / pitchMultiplier) % maxEnginePitch;
     }
