b24399e1c615fc2272d6787b4e3c34c9ec0d1faa
Timothy Nicholson <91631726+timmy-2003@users.noreply.github.com>
2023-01-22 10:57:40 +0100

    added skid sound when car is fast enough and brakes are applied long enough

--- Assets/Scripts/CarAudio.cs
+++ Assets/Scripts/CarAudio.cs
@@ -7,10 +7,13 @@ public class CarAudio : MonoBehaviour
     Rigidbody rb;
     public AudioSource engineSource;
     public AudioSource musicSource;
-
     public float minEnginePitch = 0.2f;
     public float maxEnginePitch = 3.0f;
     public float pitchMultiplier = 5.0f;
+
+    public AudioSource brakingSound;
+    public float brakingThresholdSpeed = 10f;
+    private float brakingTimer = 0f;
 
     void Start()
     {
@@ -26,5 +29,17 @@ public class CarAudio : MonoBehaviour
         }
         float speed = rb.velocity.magnitude;
         engineSource.pitch = minEnginePitch + (speed / pitchMultiplier) % maxEnginePitch;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            brakingTimer += Time.deltaTime;
+            if (brakingTimer > 0.25f && GetComponent<Rigidbody>().velocity.magnitude > brakingThresholdSpeed)
+            {
+                brakingSound.Play();
+            }
+        }
+        else
+        {
+            brakingTimer = 0f;
+        }
     }
 }
